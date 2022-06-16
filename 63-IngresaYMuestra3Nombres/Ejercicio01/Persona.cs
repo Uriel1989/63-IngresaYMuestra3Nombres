4e62a3cd5bb2af458b4e7c2a628f4750975f2cb4
Uriel Guillermo Daniel <45216219+Uriel1989@users.noreply.github.com>
2022-06-15 21:25:08 -0300

    Add files via upload

---
+++ 63-IngresaYMuestra3Nombres/Ejercicio01/Persona.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Ejercicio01
+{
+    public class Persona
+    {
+        public string Nombre { get; set; }
+        public string Apellido { get; set; }
+        public string Hablar()
+        {
+            return "Hola mi nombre es " + " " + this.Nombre + " " + this.Apellido;
+        }
+    }
+}
---
+++ 63-IngresaYMuestra3Nombres/Ejercicio01/Program.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace Ejercicio01
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            //Tipo de dato Objeto = palabra reservada constructor()
+
+            List<Persona> ListaPersonas = new List<Persona>();
+
+
+            for (int i = 0; i<3; i++)
+            {
+                Persona p1 = new Persona();
+
+                Console.WriteLine("ingrese un nombre para mostrar:");
+
+                p1.Nombre = Console.ReadLine();
+
+                Console.WriteLine("ahora ingrese el apellido:");
+                p1.Apellido = Console.ReadLine();
+
+                Console.Clear();
+
+                ListaPersonas.Add(p1);
+            }
+
+            for (int i=0; i<ListaPersonas.Count; i++)
+            {
+                Persona j = ListaPersonas[i];
+                Console.WriteLine(j.Hablar());
+            }
+            Console.ReadKey();
+        }
+    }
+}
